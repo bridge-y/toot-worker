a28f510f37fde09371a4e114212d8d33aff43d93
bridgey <bridgey4461@protonmail.ch>
2023-07-11 23:59:18 +0900

    feat: route for posting text

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,14 @@ use worker::*;
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
     // Response::ok("Hello, World!")
 
-    let req_path = env.secret("REQUEST_PATH")?.to_string();
-    Response::ok(req_path)
+    let sec_val = env.secret("REQUEST_PATH")?.to_string();
+    let req_path = format!("/{}", sec_val);
+
+    let router = Router::new();
+
+    router.post_async(&req_path, |mut req, ctx| async move {
+        // Content-Type: plain/text is expected
+        let content = req.text().await?;
+        Response::from_bytes(content.into())
+    }).run(req, env).await
 }
