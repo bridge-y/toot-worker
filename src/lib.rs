c439f5e9751ca14b93f0ba2dcc472f9a7be35f00
bridgey <bridgey4461@protonmail.ch>
2023-07-11 23:28:21 +0900

    feat: success to access to secret put by wrangler

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,8 @@ use worker::*;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    Response::ok("Hello, World!")
+    // Response::ok("Hello, World!")
+
+    let req_path = env.secret("REQUEST_PATH")?.to_string();
+    Response::ok(req_path)
 }
